2f5628beb74859a27182df346eb8dfa4ca51ec1d
joshibhavya64 <joshibhavya64@gmail.com>
2023-09-20 11:07:09 +0530

    pushing code again

---
+++ src/components/Delivery.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Delivery = () => {
+  return (
+    <div className="w-full bg-white py-16 px-4">
+      <h3 className="text-orange-500 font-bold text-2xl text-center">
+        Quick Delivery App
+      </h3>
+      <div className="w-[1240px] mx-auto grid md:grid-cols-2">
+        <img
+          className="w-[550px] mx-auto my-4"
+          src="https://res.cloudinary.com/ehizeex-shop/image/upload/v1672676822/NetflixApp/FC_two_phones.6ec9a842f905769677f9_m91off.webp"
+          alt={DataTransferItemList.id}
+        />
+        <div className="flex flex-col justify-center">
+          <p className="text-[#00df9a] font-bold ">Get The App</p>
+          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
+            Limitless Convenience on-demand
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A, iure
+            corrupti sit voluptatibus expedita nostrum atque sint quaerat soluta
+            velit voluptatum maiores exercitationem est nam omnis quidem autem
+            dolor. Nam, at delectus. Eveniet hic iste vitae totam? Explicabo
+            consectetur, odio unde rerum incidunt commodi deleniti, qui atque
+            officia hic cumque molestias pariatur voluptatem dicta ratione nisi
+            veritatis beatae fugit? Culpa at qui nulla voluptate nisi harum
+            itaque sapiente labore incidunt?
+          </p>
+          <button className="bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Delivery;
